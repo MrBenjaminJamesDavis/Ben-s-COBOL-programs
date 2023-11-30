4fe49c727a1aee2ac636130b9235c802cd9c081d
Ben James Davis <152406670+MrBenjaminJamesDavis@users.noreply.github.com>
2023-11-30 12:22:23 +0000

    Update litlgame.cob
    Re-worked the code and it works properly now.

--- litlgame.cob
+++ litlgame.cob
@@ -2,45 +2,66 @@
        IDENTIFICATION DIVISION. 
        PROGRAM-ID. litl-game.
        AUTHOR. ben-james-davis.
-       DATE-WRITTEN. 29th-november-2023.
+       DATE-WRITTEN. 30th-november-2023.
 
        DATA DIVISION.
         WORKING-STORAGE SECTION. 
-         01 play-game PIC A(1) VALUE 'Y'.
+      * A '0' indicates a boolean 'False' and a '1' indicates a boolean
+      * 'True'.
+
+      * Boolean values:
+         01 play-game PIC 9 VALUE 1.
          01 correct-guess PIC 9 VALUE 0.
-         01 guess-count PIC 99 VALUE 0.
-         01 guess PIC 9 VALUE ZERO.
+         01 first-iteration PIC 9 VALUE 1.
+
+      * Numerical values:
          01 my-number PIC 9 VALUE 5.
+         01 guess-count PIC 9 VALUE 1.
 
-       PROCEDURE DIVISION.
-
-         BB-PARA.
-           DISPLAY "Let's play a little game. I think of a number".
-           DISPLAY "between 1 and 8 and you have to guess it!".
-           DISPLAY "OK, I picked a number.".
-           MOVE 0 TO correct-guess.
-           MOVE 0 TO guess-count.
-           PERFORM CC-PARA UNTIL correct-guess NOT = 0.
-     
-         CC-PARA.
-           DISPLAY "Enter guess #" guess-count.
-           ACCEPT guess.
-           IF guess = my-number THEN
-              MOVE 1 TO correct-guess
-              MOVE 0 TO guess-count 
-              DISPLAY "You are right, my number is " my-number
-           ELSE
-              ADD 1 TO guess-count
-              IF guess > my-number THEN
-                 DISPLAY "Too big!"
-              ELSE IF guess < my-number THEN
-                 DISPLAY "Too small!"
-              END-IF
-           END-IF.
-
+      * Numerical values initialized as Null values:
+         01 guess PIC 9 VALUE ZERO.
+                 
+       PROCEDURE DIVISION.     
          AA-PARA.
-           DISPLAY "Would you like to play another game? (Y/N)".
-           ACCEPT play-game.
-           PERFORM BB-PARA UNTIL play-game NOT = 'Y'.
+           PERFORM BB-PARA UNTIL play-game = 0
 
            STOP RUN.
+
+         BB-PARA.
+           IF first-iteration = 1 THEN
+              DISPLAY "Would you like to play a game?"
+              DISPLAY "(0 = No, 1 = Yes)"
+              ACCEPT play-game
+              MOVE 0 TO first-iteration
+              IF play-game NOT = 0 THEN         
+               PERFORM CC-PARA UNTIL play-game = 0
+               MOVE 1 TO play-game 
+              END-IF
+           ELSE
+              MOVE 0 TO play-game
+           END-IF.
+           
+         CC-PARA.
+           DISPLAY "I think of a number between 1 and 8. Can you guess".
+           DISPLAY "my number?"
+           MOVE 0 TO correct-guess.
+           MOVE 1 TO guess-count.
+           PERFORM DD-PARA UNTIL correct-guess = 1.
+           DISPLAY "Would you like to keep playing the game?".
+           DISPLAY "(0 = No, 1 = Yes)".
+           ACCEPT play-game.
+
+         DD-PARA.
+           DISPLAY "Enter guess #" guess-count.
+           ADD 1 TO guess-count.
+           ACCEPT guess.
+           IF guess = my-number THEN
+              DISPLAY "Your guess is correct!"
+              MOVE 1 TO correct-guess
+           ELSE
+              IF guess < my-number  THEN         
+                 DISPLAY "Your guess is too low. . ."
+              ELSE IF guess > my-number THEN
+                 DISPLAY "Your guess is too high. . ."
+              END-IF
+           END-IF.
