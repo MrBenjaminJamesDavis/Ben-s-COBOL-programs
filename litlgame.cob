7e78bb7ac585c0ca1b5a36d0a95d72cf04eee13e
Ben James Davis <152406670+MrBenjaminJamesDavis@users.noreply.github.com>
2023-12-04 13:12:11 +0000

    Update litlgame.cob
    Made code more readable

--- litlgame.cob
+++ litlgame.cob
@@ -1,67 +1,56 @@
       *SOURCE FORMAT FREE
-       IDENTIFICATION DIVISION. 
-       PROGRAM-ID. litl-game.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. reading-files.
        AUTHOR. ben-james-davis.
        DATE-WRITTEN. 30th-november-2023.
 
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL. 
+           SELECT my-data-file ASSIGN TO "my-data.txt"
+              ORGANIZATION IS LINE SEQUENTIAL.
+
        DATA DIVISION.
-        WORKING-STORAGE SECTION. 
-      * A '0' indicates a boolean 'False' and a '1' indicates a boolean
-      * 'True'.
+       FILE SECTION.
+        FD my-data-file.       
+        01 line-one.
+         02 first-name   PIC X(10).
+         02 last-name    PIC X(15).
+         02 age          PIC 999.
+         02 city         PIC X(15).
+       WORKING-STORAGE SECTION. 
+        01 end-of-file   PIC A(1) VALUE 'F'.
+        01 lines-in-file PIC 9 VALUE ZERO.
 
-      * Boolean values:
-         01 play-game PIC 9 VALUE 1.
-         01 correct-guess PIC 9 VALUE 0.
-         01 first-iteration PIC 9 VALUE 1.
-
-      * Numerical values:
-         01 my-number PIC 9 VALUE 5.
-         01 guess-count PIC 9 VALUE 1.
-
-      * Numerical values initialized as Null values:
-         01 guess PIC 9 VALUE ZERO.
-                 
-       PROCEDURE DIVISION.     
-         AA-PARA.
-           PERFORM BB-PARA UNTIL play-game = 0
+       PROCEDURE DIVISION.
+           PERFORM OPEN-FILE-PARA.
+           PERFORM READ-FILE-PARA UNTIL end-of-file = 'T'.
+           PERFORM CLOSE-FILE-PARA.
 
            STOP RUN.
+       
+       OPEN-FILE-PARA.
+           OPEN INPUT my-data-file.
+           READ my-data-file
+              AT END
+                 MOVE 'T' TO end-of-file
+              NOT AT END
+                 COMPUTE lines-in-file = lines-in-file + 1
+           END-READ.
 
-         BB-PARA.
-           IF first-iteration = 1 THEN
-              DISPLAY "Would you like to play a game?"
-              DISPLAY "(0 = No, 1 = Yes)"
-              ACCEPT play-game
-              MOVE 0 TO first-iteration
-              IF play-game NOT = 0 THEN         
-               PERFORM CC-PARA UNTIL play-game = 0
-               MOVE 1 TO play-game 
-              END-IF
-           ELSE
-              MOVE 0 TO play-game
-           END-IF.
-           
-         CC-PARA.
-           DISPLAY "I think of a number between 1 and 8. Can you guess".
-           DISPLAY "my number?"
-           MOVE 0 TO correct-guess.
-           MOVE 1 TO guess-count.
-           PERFORM DD-PARA UNTIL correct-guess = 1.
-           DISPLAY "Would you like to keep playing the game?".
-           DISPLAY "(0 = No, 1 = Yes)".
-           ACCEPT play-game.
+       READ-FILE-PARA.
+           DISPLAY "First name: " first-name.
+           DISPLAY "Last name: " last-name.
+           DISPLAY "Age: " age.
+           DISPLAY "City: " city.
 
-         DD-PARA.
-           DISPLAY "Enter guess #" guess-count.
-           ADD 1 TO guess-count.
-           ACCEPT guess.
-           IF guess = my-number THEN
-              DISPLAY "Your guess is correct!"
-              MOVE 1 TO correct-guess
-           ELSE
-              IF guess < my-number  THEN         
-                 DISPLAY "Your guess is too low. . ."
-              ELSE IF guess > my-number THEN
-                 DISPLAY "Your guess is too high. . ."
-              END-IF
-           END-IF.
+           READ my-data-file
+              AT END
+                 MOVE 'T' TO end-of-file
+              NOT AT END
+                 COMPUTE lines-in-file = lines-in-file + 1
+           END-READ.
+
+       CLOSE-FILE-PARA.
+           DISPLAY "Number of lines: " lines-in-file.
+           CLOSE my-data-file.
